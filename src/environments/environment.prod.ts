640049ea31a304674fdce54b76a2b62e1ef97bda
Jeferson Cayo <jeferson.cayo@hacom-tech.com>
2025-07-31 10:11:34 -0500

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://bcpchallenge-fbdwgndyf5hjd6ch.centralus-01.azurewebsites.net/api/conversation'
+  apiUrl: 'https://bcpchallenge-fbdwgndyf5hjd6ch.centralus-01.azurewebsites.net/api/chat/conversation'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://bcpchallenge-fbdwgndyf5hjd6ch.centralus-01.azurewebsites.net/api/conversation'
+  apiUrl: 'https://bcpchallenge-fbdwgndyf5hjd6ch.centralus-01.azurewebsites.net/api/chat/conversation'
 };
